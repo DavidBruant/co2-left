8859fab373c7db1aaabad7c80c24a7d7aa1eb0ab
David Bruant <davidbruant@protonmail.com>
2022-03-07 22:32:24 +0100

    Avancée

--- README.md
+++ README.md
@@ -36,3 +36,27 @@ npm run build
 ```
 
 > You can preview the built app with `npm run preview`, regardless of whether you installed an adapter. This should _not_ be used to serve your app in production.
+
+### Apprentissage en vrac
+
+Objectif : 2560 GtCO2
+Uniquement avec les émissions depuis 1750 : 1696 GtCO2
+
+émissions + land use change depuis 1850 : 2431 GtCO2
+
+émissions 1750 - 1850 : 5 GtCO2
+land use change 1750 - 1850 (regression linéaire entre 1850 et 1900) : 101 GtCO2
+
+2431 + 5 + 101 = 2537, close enough
+
+Par 10 ans : 
+- 396 (15%) pour 2011 - 2020
+- 340 (13%) pour 2001 - 2010
+- 286 (11%) pour 1991 - 2000
+
+Par 20 ans :
+- 737 (29%) pour 2001 - 2020
+- 538 (21%) pour 1981 - 2000
+- 395 (15%) pour 1961 - 1980
+- 243 (9%) pour 1941 - 1960
+- 173 (7%) pour 1921 - 1940
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,26 @@
 <script>
     // @ts-ignore
     import {sum} from 'd3-array';
+    import {csv} from 'd3-fetch';
+
+    const WORLD_CODE = 'OWID_WRL'
+
+    //throw `TODO utiliser les autres données`
+
+    csv('static/data/annual-co2-emissions-per-country.csv')
+    .then(rows => {
+        const worldRows = rows
+            .filter(({Code}) => Code === WORLD_CODE)
+            .map( ({Year, "Annual CO2 emissions": CO2emissions}) => ({
+                Year: Number(Year),
+                CO2emissions: Number(CO2emissions),
+            }) )
+        
+        console.log('worldRows', worldRows)
+
+        console.log('somme', sum(worldRows.map( ({CO2emissions}) => CO2emissions )))
+
+    })
 
     let co2PerYear = {
         "2021": 10,
@@ -20,11 +40,30 @@
 <h1>CO2-left</h1>
 <p>La quantité de CO<sub>2</sub> déja dans l'atmosphère et le budget restant</p>
 
+
+
+
 <div class="co2-budget">
     {#each entries as [year, co2quantity]}	
     <div style={`width: ${co2quantity*100/total}%;`} title={year}>{year}</div>
 	{/each}
 </div>
+
+<p class="methodology-credit">Sources de données : 
+    https://ourworldindata.org/grapher/annual-co2-emissions-per-country?tab=chart&country=~OWID_WRL
+    J'ai cliqué sur "download" le 27 décembre 2021 et téléchargé le csv. Je n'utilise que les données agregées "World" (monde entier) 
+    et j'ignore les données spécifiques des pays
+    
+    ourworldindata utilise les données du Global Carbon Budget https://doi.org/10.18160/gcp-2021
+    Si j'ai bien compris, ces données sont sous licence CC-BY et un bloc 
+    illisible de noms de personnes à citer est ici : https://essd.copernicus.org/preprints/essd-2021-386/
+    
+    Les données téléchargées depuis ourworldindata ont été retraitées
+    Tout est sur github je crois https://github.com/owid
+    et j'ai la flemme de retrouver le chemin
+    
+    </p>
+
 
 <style lang="scss">
     
@@ -51,6 +90,11 @@
         }
     }
 
+    .methodology-credit{
+        margin-top: 4rem;
+        color: #444;
+        font-size: 0.8em;
+    }
 
 
 </style>
